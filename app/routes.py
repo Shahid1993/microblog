7471c3ccb7e149f77780618fa9d50a97b4929a22
Shahid1993 <shahid.bhat1994@gmail.com>
2019-05-09 19:23:31 +0530

    Loops

--- app/routes.py
+++ app/routes.py
@@ -6,4 +6,14 @@ from flask import render_template
 
 def index():
     user={'username':'Shahid'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
